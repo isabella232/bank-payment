1d3d24759d4edcb6c8bdfe117be1b58747c4c130
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-06-18 09:05:49 +0200

    ADD 13.0 account_invoice_select_for_payment
    This module allows to mark invoices as "selected for payment". This can be done
    in the list view of invoices using a button in the first column of the view
    which shows the selection status. This selection persists until a payment is
    registered.

---
+++ account_invoice_select_for_payment/models/account_move.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    selected_for_payment = fields.Boolean('To Pay')
+
+    def action_select_for_payment(self):
+        self.filtered(lambda rec: not rec.selected_for_payment).write(
+            {'selected_for_payment': True}
+        )
+
+    def action_unselect_for_payment(self):
+        self.filtered(lambda rec: rec.selected_for_payment).write(
+            {'selected_for_payment': False}
+        )
